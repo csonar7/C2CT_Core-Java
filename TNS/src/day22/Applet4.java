bd5cdac1317fd459e21569754a69cd8f57d27103
LENOVO <LENOVO@DESKTOP-197U0KE>
2022-03-29 17:03:50 +0530

    commit first

--- TNS/src/day22/Applet4.java
+++ TNS/src/day22/Applet4.java
@@ -11,7 +11,7 @@ public class Applet4 extends Applet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;//by clicking warning 
-	TextField t1,t2,t3;
+	TextField t1,t2,t3,t4;
 	Label l;
 	Button b;
 	public void init()
@@ -22,6 +22,12 @@ public class Applet4 extends Applet {
 		add(b = new Button("="));
 		add(t3 = new TextField(15));
 		
+		add(t1 = new TextField(10));
+		add(l = new Label("-"));
+		add(t2 = new TextField(5));
+		add(b = new Button("="));
+		add(t3 = new TextField(15));
+		add(t4 = new TextField(15));
 	}
 	public boolean action(Event e, Object o)
 	{
@@ -30,8 +36,19 @@ public class Applet4 extends Applet {
 		String s2 = t2.getText();
 		int j = Integer.parseInt(s2);
 		int k = i+j;
+		//int m =i*j;
+		
+		//int d = i/j;
 		String s3 = String.valueOf(k);
 		t3.setText(s3);
+		
+		String s4 = t1.getText();
+		int l = Integer.parseInt(s1);
+		String s5 = t2.getText();
+		int m = Integer.parseInt(s2);
+		int s =i-j;
+		String s6 = String.valueOf(s);
+		t4.setText(s6);
 		return true;
 	}
 	
